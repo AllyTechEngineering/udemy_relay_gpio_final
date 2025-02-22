8f65a0a868487be7dc6b805c1bc28d4ae4fea606
Bob Taylor <btaylor@allytechllc.com>
2025-02-22 11:58:27 -0500

    refactored and cleaned up code

--- lib/relay_screen/relay_screen.dart
+++ lib/relay_screen/relay_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-
-//TODO: Import the Relay_service.dart file
 import 'package:udemy_relay_gpio/relay_service/relay_service.dart';
 
 class RelayScreen extends StatefulWidget {
@@ -11,50 +9,8 @@ class RelayScreen extends StatefulWidget {
 }
 
 class _RelayScreenState extends State<RelayScreen> {
-  //TODO: Create an instance of the RelayService class
   final RelayService relayService = RelayService();
-  //TODO: Call the initGpio16Output method in the initState method
-  @override
-  void initState() {
-    relayService.initGpio16Output();
-    super.initState();
-  }
 
-  //TODO: Call the disposeGpio method in the dispose method
-  @override
-  void dispose() {
-    relayService.disposeGpio();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    //TODO: Create a Scaffold widget
-    return Scaffold(
-      //TODO: Add an AppBar widget
-      appBar: AppBar(
-        title: const Text('Udemy Relay GPIO'),
-        centerTitle: true,
-      ),
-      //TODO: Add a Center widget
-      body: Center(
-        //TODO: Add a Column widget
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            //TODO: Add the getElevatedButtonRelayOn method
-            getElevatedButtonRelayOn(),
-            const SizedBox(height: 20),
-            //TODO: Add the getElevatedButtonRelayOff method
-            getElevatedButtonRelayOff(),
-          ],
-        ),
-      ),
-    );
-  }
-
-//TODO: Create the getElevatedButtonRelayOn method
   Widget getElevatedButtonRelayOn() {
     return ElevatedButton(
       onPressed: () {
@@ -69,7 +25,6 @@ class _RelayScreenState extends State<RelayScreen> {
     );
   }
 
-//TODO: Create the getElevatedButtonRelayOff method
   Widget getElevatedButtonRelayOff() {
     return ElevatedButton(
       onPressed: () {
@@ -80,6 +35,39 @@ class _RelayScreenState extends State<RelayScreen> {
       child: const Text(
         'GPIO16 Low Relay Off',
         style: TextStyle(fontSize: 50.0),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    relayService.initGpio16Output();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    relayService.disposeGpio();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Udemy Relay GPIO'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            getElevatedButtonRelayOn(),
+            const SizedBox(height: 20),
+            getElevatedButtonRelayOff(),
+          ],
+        ),
       ),
     );
   }
--- lib/relay_service/relay_service.dart
+++ lib/relay_service/relay_service.dart
@@ -1,13 +1,13 @@
-// TODO: Import Packages
+// Import Packages
 import 'package:dart_periphery/dart_periphery.dart';
 import 'package:flutter/foundation.dart';
 
 class RelayService {
-  // TODO: Define a static variable for GPIO pin 16
+  // Define a static variable for GPIO pin 16
   // ignore: prefer_typing_uninitialized_variables
   static var gpio16;
 
-  // TODO: Initialize GPIO 16 as an output
+  // Initialize GPIO 16 as an output
   void initGpio16Output() {
     try {
       gpio16 = GPIO(16, GPIOdirection.gpioDirOut);
@@ -16,13 +16,13 @@ class RelayService {
     }
   }
 
-  // TODO: Set the output level of GPIO 16
+  // Set the output level of GPIO 16
   void gpio16OutputLevel(bool outputValue) {
     debugPrint('outputValue: $outputValue');
     gpio16.write(outputValue);
   }
 
-  // TODO: Dispose of the GPIO resources
+  // Dispose of the GPIO resources
   void disposeGpio() {
     gpio16.dispose();
   }
